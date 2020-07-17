5817ccf533d83f3df563b79ba069bd5a28c023e7
carlos benites <cbenites@everis.com>
2020-07-17 09:33:44 -0500

    primer

---
+++ src/main/java/com/everis/mstransact/MstransactApplication.java
@@ -0,0 +1,13 @@
+package com.everis.mstransact;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MstransactApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MstransactApplication.class, args);
+	}
+
+}
